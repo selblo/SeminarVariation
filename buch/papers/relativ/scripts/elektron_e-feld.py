b72840d040828c0a9bfe1764975cbf141e045807
Selvin Blöchlinger <selvin.bloechlinger@ost.ch>
2024-09-10 18:35:02 +0200

    Kapitelumstrukturierung und diverse Verbesserungen

--- buch/papers/relativ/scripts/elektron_e-feld.py
+++ buch/papers/relativ/scripts/elektron_e-feld.py
@@ -21,27 +21,27 @@ plt.rcParams.update({
 # Parameters
 t = np.linspace(0, 12, 1000)  # Time vector
 
-# Function to calculate velocity vx(t) for a given beta
-def calculate_vx(beta, E_x, t):
-    K_1 = beta * const.c / np.sqrt(1 - beta**2)
-    zaehler = -const.e * E_x * t / const.m_e + K_1
-    vx = zaehler / np.sqrt(1 + (zaehler / const.c)**2)
-    return vx
+# Function to calculate velocity beta_x(t) for a given beta0
+def calculate_betax(beta0, E_x, t):
+    eta = - const.e * E_x / (const.c * const.m_e)
+    K_1 = beta0 / np.sqrt(1 - beta0**2)
+    betax = (eta * t + K_1) / np.sqrt(1 + (eta * t + K_1)**2)
+    return betax
 
-# Calculate vx for two different beta values
-vx_beta_00_1 = calculate_vx(0.0, -1e-3, t)
-vx_beta_07_1 = calculate_vx(0.7, -1e-3, t)
-vx_beta_00_05 = calculate_vx(0.0, -0.5e-3, t)
+# Calculate vx for different beta0 and E_x values
+vx_beta_00_1 = calculate_betax(0.0, -1e-3, t)
+vx_beta_07_1 = calculate_betax(0.7, -1e-3, t)
+vx_beta_00_05 = calculate_betax(0.0, -0.5e-3, t)
 
 # Create the plot
 fig, ax = plt.subplots(figsize=(6, 4))
 
-# Plot the two graphs
-ax.plot(t, vx_beta_00_1/const.c, label=r'$\beta_0 = 0.0$, $E_x=1\, \small\frac{\textrm{mV}}{\textrm{m}}$', color='blue')
-ax.plot(t, vx_beta_00_05/const.c, label=r'$\beta_0 = 0.0$, $E_x=0.5\, \small\frac{\textrm{mV}}{\textrm{m}}$', color='red')
-ax.plot(t, vx_beta_07_1/const.c, label=r'$\beta_0 = 0.7$, $E_x=1\, \small\frac{\textrm{mV}}{\textrm{m}}$', color='green')
+# Plot the graphs
+ax.plot(t, vx_beta_00_1, label=r'$\beta_0 = 0.0$, $E_x=1\, \small\frac{\textrm{mV}}{\textrm{m}}$', color='blue')
+ax.plot(t, vx_beta_00_05, label=r'$\beta_0 = 0.0$, $E_x=0.5\, \small\frac{\textrm{mV}}{\textrm{m}}$', color='red')
+ax.plot(t, vx_beta_07_1, label=r'$\beta_0 = 0.7$, $E_x=1\, \small\frac{\textrm{mV}}{\textrm{m}}$', color='green')
 
-# Set axis labels (with LaTeX math mode)
+# Set axis labels
 ax.set_xlabel(r"$t$ in s")
 ax.set_ylabel(r"$\beta_x(t)$")
 
